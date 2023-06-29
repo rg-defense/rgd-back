da6a8c0e0089b6fa1eaee05ca59e16133287548b
Sungwoo Park <codest99@gmail.com>
2023-06-29 09:12:33 +0900

    feat: Initial commit

---
+++ src/main/java/gift/rgd/back/RandomGiftDefenseBackendApplication.java
@@ -0,0 +1,13 @@
+package gift.rgd.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGiftDefenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGiftDefenseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gift/rgd/back/RandomGiftDefenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gift.rgd.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomGiftDefenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
